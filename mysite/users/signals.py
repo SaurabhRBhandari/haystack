3d371c5608024385f9bdccb45129f8ef08d56e06
SaurabhRBhandari <bhandarisourabhrahul@gmail.com>
2022-01-27 01:15:37 +0530

    User can now change his profile

--- mysite/users/signals.py
+++ mysite/users/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
-
+from django.core.exceptions import ObjectDoesNotExist
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
@@ -12,4 +12,7 @@ def create_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    try:
+        instance.profile.save()
+    except ObjectDoesNotExist:
+        Profile.objects.create(user=instance)
